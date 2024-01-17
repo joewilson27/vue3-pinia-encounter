c3902fb8f073fc1f4cef8aa716bbb301452482be
Joe Wilson <wilsonmangunsong@gmail.com>
2024-01-17 15:12:39 +0700

    style-home
    style home

--- learn-pinia/src/views/HomeView.vue
+++ learn-pinia/src/views/HomeView.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div class="count">
+      0
+    </div>
+    <div class="buttons">
+      <button>-</button>
+      <button>+</button>
+    </div>
   </div>
 </template>
+
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+
+.buttons button {
+  font-size: 40px;
+  margin: 20px;
+}
+</style>
